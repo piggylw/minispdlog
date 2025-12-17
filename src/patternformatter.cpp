d1a66637bf0d22e75fb8f326b46d6e870dd07fd2
piggylw <2821908422@qq.com>
2025-12-17 21:47:54 +0800

    [MODIFY] 性能优化

--- src/patternformatter.cpp
+++ src/patternformatter.cpp
@@ -7,6 +7,49 @@
 namespace minispdlog
 {
 
+    // 快速整数到字符串转换
+inline void fast_uint_to_str(uint64_t n, char* buffer) {
+    // 使用查表法优化小数字
+    static constexpr char digits_table[] = 
+        "0001020304050607080910111213141516171819"
+        "2021222324252627282930313233343536373839"
+        "4041424344454647484950515253545556575859"
+        "6061626364656667686970717273747576777879"
+        "8081828384858687888990919293949596979899";
+    
+    if (n < 100) {
+        if (n < 10) {
+            buffer[0] = '0' + n;
+            buffer[1] = '\0';
+        } else {
+            const char* d = digits_table + n * 2;
+            buffer[0] = d[0];
+            buffer[1] = d[1];
+            buffer[2] = '\0';
+        }
+        return;
+    }
+    
+    // 对于大数字，回退到标准方法
+    fmt::format_to(buffer, "{}", n);
+}
+
+// 快速两位数转换(用于时间格式化)
+inline void fast_two_digits(uint32_t n, char* buffer) {
+    if (n < 100) {
+        const char* d = "0001020304050607080910111213141516171819"
+                       "2021222324252627282930313233343536373839"
+                       "4041424344454647484950515253545556575859"
+                       "6061626364656667686970717273747576777879"
+                       "8081828384858687888990919293949596979899" + n * 2;
+        buffer[0] = d[0];
+        buffer[1] = d[1];
+    } else {
+        buffer[0] = '0';
+        buffer[1] = '0';
+    }
+}
+
 //普通文本
 class RawStringFormatter : public PatternFormatter::FlagFormatter
 {
@@ -29,13 +72,40 @@ private:
     std::string m_str;
 };
 
+class AggregateFormatter : public PatternFormatter::FlagFormatter
+{
+public:
+    explicit AggregateFormatter(std::string str)
+        : m_str(std::move(str))
+    {}
+
+    void format(const details::LogMsg& msg, const std::tm& time, fmt::memory_buffer& dest) override
+    {
+        dest.append(m_str.data(), m_str.data() + m_str.size());
+    }
+    
+    void addCh(char ch){ m_str += ch; }
+    void addStr(const std::string& str){ m_str += str; }
+
+private:
+    std::string m_str;
+};
+
 //%Y : 年份
 class YearFormatter : public PatternFormatter::FlagFormatter
 {
 public:
     void format(const details::LogMsg& msg, const std::tm& time, fmt::memory_buffer& dest) override
     {
-        fmt::format_to(std::back_inserter(dest), "{:04}", time.tm_year + 1900);
+        char buffer[5];
+        int year = time.tm_year + 1900;
+        // 手动展开避免 sprintf 开销
+        buffer[0] = '0' + (year / 1000);
+        buffer[1] = '0' + ((year / 100) % 10);
+        buffer[2] = '0' + ((year / 10) % 10);
+        buffer[3] = '0' + (year % 10);
+        buffer[4] = '\0';
+        dest.append(buffer, buffer + 4);
     }
 
     std::unique_ptr<PatternFormatter::FlagFormatter> clone() const override
@@ -50,7 +120,9 @@ class MonthFormatter : public PatternFormatter::FlagFormatter
 public:
     void format(const details::LogMsg& msg, const std::tm& time, fmt::memory_buffer& dest) override
     {
-        fmt::format_to(std::back_inserter(dest), "{:02}", time.tm_mon + 1);
+        char buffer[3];
+        fast_two_digits(time.tm_mon + 1, buffer);
+        dest.append(buffer, buffer + 2);
     }
 
     std::unique_ptr<PatternFormatter::FlagFormatter> clone() const override
@@ -65,7 +137,9 @@ class DayFormatter : public PatternFormatter::FlagFormatter
 public:
     void format(const details::LogMsg& msg, const std::tm& time, fmt::memory_buffer& dest) override
     {
-        fmt::format_to(std::back_inserter(dest), "{:02}", time.tm_mday);
+        char buffer[3];
+        fast_two_digits(time.tm_mday, buffer);
+        dest.append(buffer, buffer + 2);
     }   
 
     std::unique_ptr<PatternFormatter::FlagFormatter> clone() const override
@@ -81,7 +155,9 @@ class HourFormatter : public PatternFormatter::FlagFormatter
 public:
     void format(const details::LogMsg& msg, const std::tm& time, fmt::memory_buffer& dest) override
     {
-        fmt::format_to(std::back_inserter(dest), "{:02}", time.tm_hour);
+        char buffer[3];
+        fast_two_digits(time.tm_hour, buffer);
+        dest.append(buffer, buffer + 2);
     }   
 
     std::unique_ptr<PatternFormatter::FlagFormatter> clone() const override
@@ -96,7 +172,9 @@ class MinuteFormatter : public PatternFormatter::FlagFormatter
 public:
     void format(const details::LogMsg& msg, const std::tm& time, fmt::memory_buffer& dest) override
     {
-        fmt::format_to(std::back_inserter(dest), "{:02}", time.tm_min);
+        char buffer[3];
+        fast_two_digits(time.tm_min, buffer);
+        dest.append(buffer, buffer + 2);
     }   
 
     std::unique_ptr<PatternFormatter::FlagFormatter> clone() const override
@@ -111,7 +189,9 @@ class SecondFormatter : public PatternFormatter::FlagFormatter
 public:
     void format(const details::LogMsg& msg, const std::tm& time, fmt::memory_buffer& dest) override
     {
-        fmt::format_to(std::back_inserter(dest), "{:02}", time.tm_sec);
+        char buffer[3];
+        fast_two_digits(time.tm_sec, buffer);
+        dest.append(buffer, buffer + 2);
     }   
 
     std::unique_ptr<PatternFormatter::FlagFormatter> clone() const override
@@ -126,7 +206,10 @@ class ThreadIdFormatter : public PatternFormatter::FlagFormatter
 public:
     void format(const details::LogMsg& msg, const std::tm& time, fmt::memory_buffer& dest) override
     {
-        fmt::format_to(std::back_inserter(dest), "{}", msg.m_threadId);
+        char buffer[64];
+        fast_uint_to_str(static_cast<uint64_t>(msg.m_threadId), buffer);
+        size_t len = std::strlen(buffer);
+        dest.append(buffer, buffer + len);
     }   
 
     std::unique_ptr<PatternFormatter::FlagFormatter> clone() const override
@@ -141,8 +224,12 @@ class LevelShortFormatter : public PatternFormatter::FlagFormatter
 public:
     void format(const details::LogMsg& msg, const std::tm& time, fmt::memory_buffer& dest) override
     {
-        const char* levelStr = level2ShortString(msg.m_level);
-        dest.append(levelStr, levelStr + std::strlen(levelStr));
+        static constexpr std::string_view levelStrings[] = {
+            "T", "D", "I", "W", "E", "C"  // 编译时预计算
+        };
+        
+        auto levelStr = levelStrings[static_cast<size_t>(msg.m_level)];
+        dest.append(levelStr.data(), levelStr.data() + levelStr.size());
     }   
 
     std::unique_ptr<PatternFormatter::FlagFormatter> clone() const override
@@ -157,8 +244,13 @@ class LevelFullFormatter : public PatternFormatter::FlagFormatter
 public:
     void format(const details::LogMsg& msg, const std::tm& time, fmt::memory_buffer& dest) override
     {
-        const char* levelStr = level2String(msg.m_level);
-        dest.append(levelStr, levelStr + std::strlen(levelStr));
+        static constexpr std::string_view level_strings[] = {
+            "trace", "debug", "info", "warning", "error", "critical"
+        };
+
+        auto level_str = level_strings[static_cast<size_t>(msg.m_level)];
+        dest.append(level_str.data(), level_str.data() + level_str.size());
+
     }   
 
     std::unique_ptr<PatternFormatter::FlagFormatter> clone() const override
@@ -254,6 +346,8 @@ PatternFormatter::PatternFormatter(std::string pattern)
 
 void PatternFormatter::format(const details::LogMsg& msg, fmt::memory_buffer& dest)
 {
+    dest.reserve(dest.size() + 256); // 预留空间，避免多次内存分配
+
     auto secs = std::chrono::duration_cast<std::chrono::seconds>(msg.m_timePoint.time_since_epoch());
     if (secs != m_lastTimeSec)
     {
@@ -285,85 +379,59 @@ void PatternFormatter::compilePattern()
 {
     auto it = m_pattern.begin();
     auto end = m_pattern.end();
-    std::string userChars;
-
-    while(it != end)
-    {
-        if(*it == '%')
-        {
-            if(!userChars.empty())
-            {
-                m_formatters.push_back(std::make_unique<RawStringFormatter>(userChars));
-                userChars.clear();
+    std::unique_ptr<AggregateFormatter> userChars;
+    
+    while (it != end) {
+        if (*it == '%') {
+            // 保存聚合的普通文本
+            if (userChars) {
+                m_formatters.push_back(std::move(userChars));
             }
-
+            
+            // 解析占位符
             ++it;
-
-            if(it != end)
-            {
+            if (it != end) {
                 char flag = *it;
                 ++it;
-
-                switch(flag)
-                {
-                    case 'Y':
-                        m_formatters.push_back(std::make_unique<YearFormatter>());
-                        break;
-                    case 'm':
-                        m_formatters.push_back(std::make_unique<MonthFormatter>());
-                        break;
-                    case 'd':
-                        m_formatters.push_back(std::make_unique<DayFormatter>());
-                        break;
-                    case 'H':
-                        m_formatters.push_back(std::make_unique<HourFormatter>());
-                        break;
-                    case 'M':
-                        m_formatters.push_back(std::make_unique<MinuteFormatter>());
-                        break;
-                    case 'S':
-                        m_formatters.push_back(std::make_unique<SecondFormatter>());
-                        break;
-                    case 't':
-                        m_formatters.push_back(std::make_unique<ThreadIdFormatter>());
-                        break;
-                    case 'l':
-                        m_formatters.push_back(std::make_unique<LevelShortFormatter>());
-                        break;
-                    case 'L':
-                        m_formatters.push_back(std::make_unique<LevelFullFormatter>());
-                        break;
-                    case 'n':
-                        m_formatters.push_back(std::make_unique<LoggerNameFormatter>());
-                        break;
-                    case 'v':
-                        m_formatters.push_back(std::make_unique<PayloadFormatter>());
-                        break;
-                    case 'F':
-                        m_formatters.push_back(std::make_unique<SourceFileFormatter>());
-                        break;
-                    case 'f':
-                        m_formatters.push_back(std::make_unique<SourceFunctionFormatter>());
-                        break;
-                    case 'P':
-                        m_formatters.push_back(std::make_unique<SourceLineFormatter>());
+                
+                // 根据 flag 创建对应的 formatter
+                switch (flag) {
+                    case 'Y': m_formatters.push_back(std::make_unique<YearFormatter>()); break;
+                    case 'm': m_formatters.push_back(std::make_unique<MonthFormatter>()); break;
+                    case 'd': m_formatters.push_back(std::make_unique<DayFormatter>()); break;
+                    case 'H': m_formatters.push_back(std::make_unique<HourFormatter>()); break;
+                    case 'M': m_formatters.push_back(std::make_unique<MinuteFormatter>()); break;
+                    case 'S': m_formatters.push_back(std::make_unique<SecondFormatter>()); break;
+                    case 'l': m_formatters.push_back(std::make_unique<LevelShortFormatter>()); break;
+                    case 'L': m_formatters.push_back(std::make_unique<LevelFullFormatter>()); break;
+                    case 'n': m_formatters.push_back(std::make_unique<LoggerNameFormatter>()); break;
+                    case 'v': m_formatters.push_back(std::make_unique<PayloadFormatter>()); break;
+                    case 't': m_formatters.push_back(std::make_unique<ThreadIdFormatter>()); break;
+                    case '%': 
+                        if (!userChars) userChars = std::make_unique<AggregateFormatter>("");
+                        userChars->addCh('%'); 
                         break;
                     default:
-                        //未知标志，忽略
+                        // 未知占位符,创建聚合formatter
+                        if (!userChars) userChars = std::make_unique<AggregateFormatter>("");
+                        userChars->addCh('%');
+                        userChars->addCh(flag);
                         break;
                 }
             }
-        }
-        else
-        {
-            userChars += *it;
+        } else {
+            // 普通字符,累积到聚合formatter
+            if (!userChars) {
+                userChars = std::make_unique<AggregateFormatter>("");
+            }
+            userChars->addCh(*it);
             ++it;
         }
     }
-
-    if(!userChars.empty())
-    {
-        m_formatters.push_back(std::make_unique<RawStringFormatter>(userChars));
+    
+    // 处理末尾的聚合文本
+    if (userChars) {
+        m_formatters.push_back(std::move(userChars));
     }
 }
 
